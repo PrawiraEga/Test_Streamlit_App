5b0f74045507a4df2c2614370ca0d6f075e0824b
PrawiraEga <57137868+PrawiraEga@users.noreply.github.com>
2024-08-24 11:32:13 +0700

    Update app.py
    add command spacy download en_core_web_sm

--- Sanber_Final_Project/app.py
+++ Sanber_Final_Project/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import subprocess
 
 st.set_page_config(page_title="Sentiment Analysis Data App", page_icon="📊", layout="centered")
 
@@ -6,6 +7,15 @@ st.set_page_config(page_title="Sentiment Analysis Data App", page_icon="📊", l
 st.sidebar.title("Navigation")
 pages = ["Home", "Analysis"]
 page = st.sidebar.radio("Go to", pages)
+
+# CMD SpaCy download
+command = "python -m spacy download en_core_web_sm"
+    # Run CMD through subprocess
+try:
+    subprocess.run(command, check=True, shell=True)
+    print("Model 'en_core_web_sm' download succesfull.")
+except subprocess.CalledProcessError as e:
+    print(f"Error while downloading: {e}")
 
 if page == "Home":
     import pages.home as home
