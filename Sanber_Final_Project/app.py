21bddc6ce9a738da79f4421a90c587e65652418a
PrawiraEga <57137868+PrawiraEga@users.noreply.github.com>
2024-08-24 09:04:50 +0700

    Update app.py
    Delete import nltk
    Remove  function download_nltk_pkg()

--- Sanber_Final_Project/app.py
+++ Sanber_Final_Project/app.py
@@ -1,13 +1,11 @@
 import streamlit as st
-from nltk_download import download_nltk_pkg
+
 st.set_page_config(page_title="Sentiment Analysis Data App", page_icon="📊", layout="centered")
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
 pages = ["Home", "Analysis"]
 page = st.sidebar.radio("Go to", pages)
-
-download_nltk_pkg()
 
 if page == "Home":
     import pages.home as home
