41116aa8d1e3e5085a6389e6fe12221d4d6b77b8
PrawiraEga <57137868+PrawiraEga@users.noreply.github.com>
2024-08-28 18:17:25 +0700

    Update visualization.py
    Add Plotly Express

--- Sanber_Final_Project/data/visualization.py
+++ Sanber_Final_Project/data/visualization.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import plotly.express as px
+import random
 
 def senti_plot(df):
     fig, ax = plt.subplots()
@@ -10,3 +12,11 @@ def senti_plot(df):
     # Tampilkan plot di Streamlit
     st.write("### Sentiment Distribution Histogram")
     st.pyplot(fig)
+
+def plotly_chart(df, col):
+    random_int = random.randint(100000, 270000)
+    fig = px.bar(df, y = 'count', template = 'seaborn')
+    fig.update_traces(marker_color='#'+str(random_int))
+    fig.update_layout(title_text="Number of " + col,title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title="Total", xaxis_title="Sentimen")
+    
+    return fig
